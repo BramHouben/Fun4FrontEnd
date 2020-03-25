d36d308dc090aa27b04f3196c7d399d744297506
Houben,Bram B <bram.houben@student.fontys.nl>
2020-03-25 16:12:28 +0100

    test

--- gui/tests/unit/example.spec.js
+++ gui/tests/unit/example.spec.js
@@ -1,16 +1,14 @@
-import {
-  shallowMount
-} from '@vue/test-utils'
-import Mainpage from '@/components/Mainpage.vue'
+import { shallowMount } from "@vue/test-utils";
+import Mainpage from "@/components/Mainpage.vue";
 
-describe('Mainpage.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
+describe("Mainpage.vue", () => {
+  it("renders props.msg when passed", () => {
+    const msg = "new messages";
     const wrapper = shallowMount(Mainpage, {
       propsData: {
         msg
       }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+    });
+    expect(wrapper.text()).toMatch(msg);
+  });
+});
