df0a1431cd733244dbf36531a2498547151829fe
Houben,Bram B <bram.houben@student.fontys.nl>
2020-06-03 09:56:00 +0200

    test hASH

--- src/router/index.js
+++ src/router/index.js
@@ -51,10 +51,9 @@ const routes = [{
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  // base: process.env.BASE_URL,
-  // routes
-  routes: routes
+  mode: "hash",
+  base: process.env.BASE_URL,
+  routes
 });
 
 async function isAdmin(to, from, next) {
