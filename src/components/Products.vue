a0f554a1f84543db67b0869f8dfe01dd41a38782
Houben,Bram B <bram.houben@student.fontys.nl>
2020-06-08 18:49:33 +0200

    foto's

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -4,9 +4,8 @@
       <v-layout row justify-space-around>
         <v-flex md3 v-for="product in productsloaded" v-bind:key="product.id">
           <v-card max-width="400" class="cardMain">
-            <v-img class="black--text align-end" height="200px" src>
-              <v-card-title>{{ product.productName }}</v-card-title>
-            </v-img>
+            <v-img class="black--text align-end" height="150px" :src="product.picture"></v-img>
+            <v-card-title>{{ product.productName }}</v-card-title>
             <v-card-subtitle class="pb-0">{{ product.productPrice }} Euro</v-card-subtitle>
             <v-card-actions>
               <v-btn icon color="pink">
