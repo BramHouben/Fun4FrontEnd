fecff8e6e6092bb764baada7f324d68133e6dbc6
Houben,Bram B <bram.houben@student.fontys.nl>
2020-06-10 20:23:06 +0200

    update babel

--- src/components/OrderDialog.vue
+++ src/components/OrderDialog.vue
@@ -1,52 +1,57 @@
 <template>
   <v-dialog v-model="dialog" scrollable max-width="300px">
     <template v-slot:activator="{ on }">
-      <v-btn color="primary" dark v-on="on" @click="getInfo(order.id)">Details</v-btn>
+      <v-btn color="primary" dark v-on="on" @click="getInfo(order.id)"
+        >Details</v-btn
+      >
     </template>
     <v-card class="cardprops">
       <v-card-title>Detials</v-card-title>
       <v-divider></v-divider>
       <v-card-text>
-        <h3>Order id: {{order.id}}</h3>
-        <h2>Totale Prijs: {{orderdetails.totalPrice}}</h2>
+        <h3>Order id: {{ order.id }}</h3>
+        <h2>Totale Prijs: {{ orderdetails.totalPrice }}</h2>
 
         <div
           v-for="orderdetails in orderdetails.productOrderedList"
           v-bind:key="orderdetails.id"
           class="flex-table"
         >
-          <div>Naam:{{orderdetails.productName}} en aantal:{{orderdetails.count}}</div>
+          <div>
+            Naam:{{ orderdetails.productName }} en aantal:{{
+              orderdetails.count
+            }}
+          </div>
         </div>
       </v-card-text>
       <v-divider></v-divider>
     </v-card>
   </v-dialog>
 </template>
-    
-    <script>
+
+<script>
 export default {
   name: "OrderDetails",
   props: ["order"],
   data() {
     return {
       dialog: false,
-      orderdetails: []
+      orderdetails: [],
     };
   },
   methods: {
     async getInfo(orderid) {
       return await this.$store
         .dispatch("getDetails", { id: orderid })
-        .then(result => {
+        .then((result) => {
           this.orderdetails = result;
         });
-    }
-  }
+    },
+  },
   // method() {
   //   this.$store.dispatch("getDetails", { orderId: this.order.id });
   // }
 };
 </script>
-    
-    <style scoped>
-</style>
+
+<style scoped></style>
